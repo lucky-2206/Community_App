8051ab42f0402d4361660f0c53be49879f163e78
lucky_2206 <vaibhavjainlucky98@gmail.com>
2023-06-19 02:05:20 +0530

    User Authentication Done. Post Creation UI Done

--- app/src/main/java/com/example/communityapp/MainActivity.kt
+++ app/src/main/java/com/example/communityapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.communityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.e
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton:FloatingActionButton = findViewById(R.id.button_addPost)
+        addButton.setOnClickListener{
+            val intent = Intent(this,AddPost::class.java)
+           startActivity(intent)
+        }
     }
 }
